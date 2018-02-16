c8771dc586aac22ac23e8c625814cb1f7b2ca258
LuckyChucky <luckychuckynorris@gmail.com>
2018-02-15 21:28:01 -0500

    implemented working Convolution algorithm

--- src/Convolution.h
+++ src/Convolution.h
@@ -1,11 +1,24 @@
 #ifndef CONVOLUTIONALG_INCLUDE_CONVOLUTION_H_
 #define CONVOLUTIONALG_INCLUDE_CONVOLUTION_H_
 
+#include <cstddef>
 #include <iostream>
+#include <vector>
 
-double Convolution() {
+std::vector<double> Convolution(std::vector<double> x, std::vector<double> h) {
+  size_t M { x.size() };
+  size_t N { h.size() };
+  std::vector<double> output;
+  output.resize(M + N - 1);
 
-  return 0.0;
+  for (int n = 0; n <= M + N - 2; ++n) {
+    for (int k = 0; k <= N; ++k) {
+      if (n - k >= 0) {
+        output[n] += x[k] * h[n - k];
+      }
+    }
+  }
+  return output;
 }
 
 #endif
--- src/test.cc
+++ src/test.cc
@@ -1,8 +1,16 @@
 #include <Convolution.h>
+#include <cstddef>
+#include <iostream>
+#include <vector>
 
 int main() {
+  std::vector<double> arr1 { 1.2, 2.3, 3.4, 4.3 };
+  std::vector<double> arr2 { 2.1, 3.2, 4.3, 5.4, 6.5 };
+  std::vector<double> arr3 { Convolution(arr1, arr2) };
+  size_t arr3_size { arr3.size() };
 
-  std::cout << "Hello, world!" << std::endl;
-
+  for (int x = 0; x < arr3_size; ++x) {
+    std::cout << arr3[x] << std::endl;
+  }
   return 0;
 }
